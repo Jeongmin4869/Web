89e193eef70c9670880ad1cf0b3f52234fb479a5
jeongmin4869 <ljm4135@naver.com>
2022-04-26 19:31:56 +0900

    Dependency Injection(의존성 주입)

---
+++ HelloDI/src/main/java/kr/ac/hansung/spring/di/PetOwner.java
@@ -0,0 +1,15 @@
+package kr.ac.hansung.spring.di;
+
+public class PetOwner {
+	
+	private AnimalType animal;
+	
+	public PetOwner(AnimalType animal) {
+		this.animal = animal;
+	}
+
+	public void play() {
+		animal.sound();
+	}
+	
+}
