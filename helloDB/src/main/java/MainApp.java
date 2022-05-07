22ce3a12f30db3135a2aac3878ae73ff55f5b00d
jeongmin4869 <ljm4135@naver.com>
2022-05-07 17:57:57 +0900

    DAO(Data Access Object) / annotation @Component @Autowierd

--- helloDB/src/main/java/MainApp.java
+++ helloDB/src/main/java/MainApp.java
@@ -1,11 +1,15 @@
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import kr.ac.hansung.spring.csemall.OfferDAO;
+
 public class MainApp {
 
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = 
 				new ClassPathXmlApplicationContext("kr/ac/hansung/spring/csemall/beans/beans.xml");
 	
+		OfferDAO offerDAO = (OfferDAO)context.getBean("offerDAO");
+		System.out.println("The record count is " + offerDAO.getRowCount());
 		context.close();
 	}
 
--- helloDB/src/main/java/kr/ac/hansung/spring/csemall/OfferDAO.java
+++ helloDB/src/main/java/kr/ac/hansung/spring/csemall/OfferDAO.java
@@ -1,12 +1,17 @@
 package kr.ac.hansung.spring.csemall;
 
 import javax.sql.DataSource;
-import org.springframework.jdbc.core.JdbcTemplate;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class OfferDAO {
 
 	private JdbcTemplate jdbcTemplateObject;
 	
+	@Autowired
 	public void setDataSource(DataSource dataSource) {
 		this.jdbcTemplateObject = new JdbcTemplate(dataSource);
 	}
