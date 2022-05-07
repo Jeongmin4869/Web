c0a273a7ce7c033dfe16b2248418caf3ce015952
jeongmin4869 <ljm4135@naver.com>
2022-05-07 13:53:16 +0900

    DAO(Data Access Object)

--- helloDB/src/main/java/MainApp.java
+++ helloDB/src/main/java/MainApp.java
@@ -1,9 +1,12 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("kr/ac/hansung/spring/csemall/beans/beans.xml");
+	
+		context.close();
 	}
 
 }
