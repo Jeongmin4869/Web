fdc2a381eb13f3ac94b2e1c669f20b1efdda5a97
jeongmin4869 <ljm4135@naver.com>
2022-05-09 20:17:53 +0900

    DAO(Data Access Object) / anonymous class(익명 클래스)

--- helloDB/src/main/java/kr/ac/hansung/spring/csemall/OfferDAO.java
+++ helloDB/src/main/java/kr/ac/hansung/spring/csemall/OfferDAO.java
@@ -1,31 +1,48 @@
 package kr.ac.hansung.spring.csemall;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OfferDAO {
 
 	private JdbcTemplate jdbcTemplateObject;
-	
+
 	@Autowired
 	public void setDataSource(DataSource dataSource) {
 		this.jdbcTemplateObject = new JdbcTemplate(dataSource);
 	}
-	
+
 	public int getRowCount() {
 		String sqlStatement = "select count(*) from offers";
 		return jdbcTemplateObject.queryForObject(sqlStatement, Integer.class);
 	}
-	
-	//querying and returning a single object
+
+	// querying and returning a single object
 	public Offer getOffer(String name) {
 		String sqlStatement = "select * from offers where name=?";
-		
-		return jdbcTemplateObject.queryForObject(sqlStatement,new Object[] {name},
-				new RowMapper<Offer>());
+
+		return jdbcTemplateObject.queryForObject(sqlStatement, new Object[] {name}, 
+				new RowMapper<Offer>() {
+						@Override
+						public Offer mapRow(ResultSet rs, int rowNum) throws SQLException {
+							
+							Offer offer = new Offer();
+							
+							offer.setId(rs.getInt("id"));
+							offer.setName(rs.getString("name"));
+							offer.setEmail(rs.getString("email"));
+							offer.setText(rs.getString("text"));
+							
+							return offer;
+						}
+		});
 	}
 }
