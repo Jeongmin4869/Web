e0dbeb67e7a66196d81a2bfbc118cd0dc117b52f
jeongmin4869 <ljm4135@naver.com>
2022-06-29 06:25:18 +0900

    Hibernate Basics

--- testHibernate/src/main/java/testHibernate/Product.java
+++ testHibernate/src/main/java/testHibernate/Product.java
@@ -1,5 +1,33 @@
 package testHibernate;
 
-public class Product {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+/*******************
+ * 
+ * Hibernate가 이 class에 해당되는 내용을 table로 만들어준다.
+ * name을 사용해 table의 이름 지정
+ * Entity bean으로 등록
+ *
+ ******************/
+@Entity(name="Product2") 
+public class Product {
+	
+	@Id // primary key
+	@GeneratedValue // 자동생성
+	@Column(name="product_id")
+	private int id;
+	
+	private String name;
+	
+	private int price;
+	
+	private String description;
+	
 }
---
+++ testHibernate/src/main/java/testHibernate/testMain.java
@@ -0,0 +1,38 @@
+package testHibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class testMain {
+	
+	private static SessionFactory sessionFactory;
+	
+	public static void main(String[] args) {
+
+		/*		
+ 		Configuration conf = new Configuration();
+		conf.configure("hibernate.cfg.xml");
+		sessionFactory = conf.buildSessionFactory();
+		*/
+		
+		sessionFactory = new Configuration().configure().buildSessionFactory();
+		
+		Product product = new Product();
+		product.setName("notebook");
+		product.setPrice(2000);
+		product.setDescription("Awesome notebook!!");
+		
+		Session session = sessionFactory.openSession();
+		Transaction tx = session.beginTransaction();
+		
+		// 객체를 저장하여 DB에 반영. sql statement 를 대신함.
+		session.save(product);
+		
+		tx.commit();
+		session.close();
+		
+	}
+
+}
