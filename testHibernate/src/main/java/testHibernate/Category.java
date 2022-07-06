c6fe1ddc4a10dd599329226ddfdd6cec7567f3bb
jeongmin4869 <ljm4135@naver.com>
2022-07-06 21:44:11 +0900

    Hibernate (One To Many)

---
+++ testHibernate/src/main/java/testHibernate/Category.java
@@ -0,0 +1,21 @@
+package testHibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Category {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private String name;
+	
+}
