974f06688faae55756554de049fbecd8e9c26629
jeongmin4869 <ljm4135@naver.com>
2022-07-10 19:48:43 +0900

    Hibernate With Spring (Set Service, Controller)

--- eStore/src/main/java/kr/ac/hansung/cse/controller/AdminController.java
+++ eStore/src/main/java/kr/ac/hansung/cse/controller/AdminController.java
@@ -63,8 +63,7 @@ public class AdminController {
 			return "addProduct";
 		}
 
-		if(!productService.addProduct(product))
-			System.out.println("Adding product cannot be done");
+		productService.addProduct(product);
 		return "redirect:/admin/productInventory";
 		
 	}
@@ -72,9 +71,10 @@ public class AdminController {
 	@RequestMapping(value = "/productInventory/deleteProduct/{id}",  method=RequestMethod.GET)
 	public String deleteProduct(@PathVariable int id) {
 		
-		if(!productService.deleteProduct(id)) {
-			System.out.println("Deleting product cannot be done");
-		}
+		Product product = productService.getProductById(id);
+		
+		productService.deleteProduct(product);
+		
 		return "redirect:/admin/productInventory";
 	}
 	
@@ -100,8 +100,7 @@ public class AdminController {
 			return "updateProduct";
 		}
 
-		if(!productService.updateProduct(product))
-			System.out.println("Updating product cannot be done");
+		productService.updateProduct(product);
 		return "redirect:/admin/productInventory";
 		
 	}
