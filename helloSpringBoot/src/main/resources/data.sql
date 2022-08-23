da9e6e2aa4466c707f4a0da9b25039702c10cf34
jeongmin4869 <ljm4135@naver.com>
2022-08-23 21:48:03 +0900

    Spring Boot (데이터 삽입 쿼리)

---
+++ helloSpringBoot/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into customer(firstname, lastname) values ("alice", "kim")
+insert into customer(firstname, lastname) values ("bob", "kim")
+insert into customer(firstname, lastname) values ("charlie", "lee")
+
